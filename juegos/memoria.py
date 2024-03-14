a878e401f5e9fcf9217b3ab8d4da2ce88046431f
Juan Manuel Hernández <77511033+JuanHernandez-uc@users.noreply.github.com>
2024-03-14 21:36:16 +0000

    juego casi listo

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,4 @@
+import random
+
 def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
-    pass
+    print(random.randint(1, 1000))
